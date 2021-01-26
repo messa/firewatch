7b17cb436cfd560a5bee0b4a540faf7ed7899a54
Petr Messner <petr.messner@gmail.com>
2021-01-26 14:34:55 +0100

    frontend: hello world

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function IndexPage() {
+  return (
+    <Layout>
+      <h1>Firewatch</h1>
+      <p>Hello World!</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
