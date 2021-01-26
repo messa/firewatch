c829e808902cfe489ae37e0f36cae489ab71b17c
Petr Messner <petr.messner@gmail.com>
2021-01-26 18:27:40 +0100

    frontend: show check results

--- frontend/components/CheckList.js
+++ frontend/components/CheckList.js
@@ -14,7 +14,9 @@ function CheckList() {
       {error && <p>Failed to load</p>}
       {data && data.http_checks.map((check, i) => (
         <div key={i}>
+          <p>Id: <code>{check.check_id}</code></p>
           <p>URL: <code>{check.url}</code></p>
+          <p>Last result: <code>{JSON.stringify(check.last_result)}</code></p>
         </div>
       ))}
     </div>
