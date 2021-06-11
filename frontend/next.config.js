c925830f24d5f9f3f1978e1192b8c15f7cd060b5
Petr Messner <petr.messner@gmail.com>
2021-06-11 22:50:10 +0200

    frontend: add webpack5: true to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8000/api/:path*'
-        }
-      ]
-    }
+  future: {
+    webpack5: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*'
+      }
+    ]
   }
+}
